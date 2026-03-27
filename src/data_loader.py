d9db81bb63422eb7a8cc142d3b19a9907e45e81d
Rishikesh Beliya <rishikeshbeliya@gmail.com>
2026-03-27 16:59:11 +0530

    Minor errors fixed. It is now fast and scaleable

--- src/data_loader.py
+++ src/data_loader.py
@@ -20,8 +20,6 @@ keywords["id"] = keywords["id"].dropna().astype(int)
 # Todo: Creating content based filtering DF
 content_based_df = pd.merge(movies, keywords, on="id", how="inner")
 
-content_based_df.to_csv("../data/content_based_df_new.csv", index=False)
-
 
 #!==============================
 #! Collaborative Filtering
@@ -31,16 +29,10 @@ links = pd.read_csv("../data/links_small.csv", low_memory=False)
 collaborative_df = pd.merge(ratings, links, how="inner", on="movieId")
 collaborative_df = collaborative_df.drop(columns=["timestamp", "imdbId"])
 
-collaborative_df.to_csv("../data/collaborative_df_new.csv", index=False)
-
-
-#!==============================
-#! Hybrid Filtering
-#!==============================
-hybrid_based_df = pd.merge(
-    collaborative_df, content_based_df, left_on="tmdbId", right_on="id"
+collaborative_df.to_csv("../data/collaborative_df.csv", index=False)
+content_based_df = content_based_df.rename(
+    columns={
+        "id": "movieId",
+    }
 )
-
-hybrid_based_df = hybrid_based_df.drop(columns=["tmdbId"])
-
-hybrid_based_df.to_csv("../data/hybrid.csv", index=False)
+content_based_df.to_csv("../data/content_based_df.csv", index=False)
